2f92d8969412a973fdfdeae623f8b590c486cadf
vedant-workspaces <vedantworkspaces@gmail.com>
2026-01-23 12:54:43 +0530

    add getProgramNames method to ProgramController and corresponding repository and service methods; update api_v1 routes to include new endpoint for retrieving program names

--- app/Repositories/V1/ProgramRepository.php
+++ app/Repositories/V1/ProgramRepository.php
@@ -7,4 +7,6 @@ use App\Repositories\Dao\V1\ProgramDao;
 interface ProgramRepository
 {
     public function createProgram(ProgramDao $programDao): int;
+
+    public function getProgramNames(): array;
 }
